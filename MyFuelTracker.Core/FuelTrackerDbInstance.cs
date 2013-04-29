b4c2f2f955ac6ce1288a02fdf2234d9b5a31ea84
Oleg Gozhyi <Oleg.gozhyi@outlook.com>
2013-04-29 19:07:41 +0300

    added core project

---
+++ MyFuelTracker.Core/FuelTrackerDbInstance.cs
@@ -0,0 +1,27 @@
+﻿using MyFuelTracker.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Wintellect.Sterling.Database;
+
+namespace MyFuelTracker.Core
+{
+	public class FuelTrackerDbInstance : BaseDatabaseInstance
+	{
+		protected override List<ITableDefinition> RegisterTables()
+		{
+			return new List<ITableDefinition>
+			{
+				CreateTableDefinition<Fillup, int>(f => f.Id)
+			};
+		}
+	}
+}
